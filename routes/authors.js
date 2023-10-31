fc7f916fd87fa51f9d4966a02f1609232f415e7f
AndySama79 <arnab.dey@plaksha.edu.in>
2023-10-31 19:51:52 +0530

    added partials

--- routes/authors.js
+++ routes/authors.js
@@ -3,8 +3,19 @@ const router = express.Router()
 const Author = require('../models/authors')
 
 // all authors route
-router.get('/', (req, res) => {
-    res.render('authors/index');
+router.get('/', async (req, res) => {
+    let searchOptions = {}
+    if (req.query.name != null && req.query.name !== '') {
+        searchOptions.name = new RegExp(req.query.name, 'i') // i means case insensitive
+    }
+    try {
+        const authors = await Author.find(this.searchOptions)
+        res.render('authors/index', { 
+            authors: authors, 
+            searchOptions: req.query })
+    } catch {
+        res.redirect('/')
+    }
 });
 
 // new author route
